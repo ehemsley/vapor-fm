f495182f07db28662cd8f81ed9a25cbe18b83a63
Evan Hemsley <evan.hemsley@gmail.com>
2018-02-06 14:00:18 -0800

    upgradin

--- app/js/visualizers/shader_visualizers/checkerboard_visualizer.js
+++ app/js/visualizers/shader_visualizers/checkerboard_visualizer.js
@@ -4,13 +4,10 @@ module.exports = class CheckerboardVisualizer extends ShaderVisualizer {
   constructor (audioInitializer, renderer) {
     super(audioInitializer, renderer)
 
-    console.log('eyyy')
-    // load resource here
     super.LoadResource('shaders/checkerboard.glsl', (responseText) => {
       this.quadMesh.material.fragmentShader = responseText
       // this.quadMesh.material.map.needsUpdate = true
       this.quadMesh.material.needsUpdate = true
-      console.log(responseText)
     })
   }
 }
